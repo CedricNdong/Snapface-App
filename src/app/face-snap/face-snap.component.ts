fdbb77634b36153fe83292c20d766278b8f28deb
CedricNdong <juniorndong901@gmail.com>
2022-06-30 05:59:05 +0200

    app update -- first component created

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit{
+
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit() {
+    this.title = 'Naruto';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://images5.alphacoders.com/532/thumb-1920-532559.jpg';
+
+  }
+
+
+
+
+}
